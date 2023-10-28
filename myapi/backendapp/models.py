199b2074530a83359c5a0b0c4a236a61881d041e
kutasz <lukasz.janczak.pl@gmail.com>
2023-10-28 14:25:14 +0200

    Added Database

--- myapi/backendapp/models.py
+++ myapi/backendapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    role = models.CharField(max_length=16)
+
+
+class Price(models.Model):
+    id = models.IntegerField(primary_key=True)
+    amount = models.FloatField()
+    quantity = models.IntegerField()
+    weight = models.FloatField()
+    per_kg = models.BooleanField()
+
+
+class Product(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=64)
+
+
+class Post(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    price_id = models.ForeignKey(to=Price, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+
+
+
+
+
+
 # Create your models here.
--- myapi/backendapp/views.py
+++ myapi/backendapp/views.py
@@ -3,3 +3,18 @@ from django.http import JsonResponse
 
 def ping(request):
     return JsonResponse({'message': "dziala w chuj"})
+
+
+def products(request):
+    return JsonResponse(
+        {
+        'status': 200,
+        'products': [
+            {'id': 1, 'name': 'gowno', 'description': 'chuj'},
+            {'id': 2, 'name': 'kupa', 'description': 'chuj'},
+            {'id': 3, 'name': 'dupa', 'description': 'chuj'}
+        ]
+    }
+    )
+
+    
