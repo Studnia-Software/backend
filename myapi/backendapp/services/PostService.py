bb508b94e9c66d1e812227be233682f58a53b442
wiktorsk8 <wikto4k@gmail.com>
2023-10-28 14:32:27 +0200

    dobre zmiany pzdr

---
+++ myapi/backendapp/services/PostService.py
@@ -0,0 +1,6 @@
+
+class PostService:
+
+    def store(self, arr):
+        title = arr['title']
+        product_name = arr['product'] 
--- myapi/backendapp/views.py
+++ myapi/backendapp/views.py
@@ -3,3 +3,37 @@ from django.http import JsonResponse
 
 def ping(request):
     return JsonResponse({'message': "dziala w chuj"})
+
+
+def store_post(request):
+    if request.method == 'POST':
+        try:
+            # Check if the Content-Type is application/json
+            if 'application/json' in request.content_type:
+                # Parse the JSON data
+                json_data = json.loads(request.body.decode('utf-8'))
+                
+                # Now, json_data is a Python dictionary containing your JSON data
+                # You can access it like any other dictionary
+                title = json_data.get('title')
+                product = json_data.get('product')
+                user_id = json_data.get('user_id')
+                price = json_data.get('price')
+
+                
+                # Do something with the data
+                
+                return JsonResponse({'message': 'Success'})
+            else:
+                return JsonResponse({'error': 'Invalid Content-Type'}, status=400)
+        except json.JSONDecodeError as e:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+    else:
+        return JsonResponse({'error': 'Invalid Request Method'}, status=405)
+
+
+
+
+
+
+    
